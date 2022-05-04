30da699ca101c3f24d14632f95a4bca854d8082b
shubham2o <shubhamsharma.ha@gmail.com>
2022-05-04 10:39:09 +0530

    scrollTo() and onscroll event added

--- Doubts/doubts.js
+++ Doubts/doubts.js
@@ -9,3 +9,29 @@ function scrollHor() {
 function scrollVer() {
     window.scrollTo(0, 500);
 }
+
+
+// onScroll
+let a = 0;
+
+function onScroll() {
+    document.getElementById("demo").innerHTML = a += 1;
+}
+
+function onScroll2() {
+    document.getElementById("demo2").innerHTML = "You scrolled in div.";
+}
+
+document.getElementById("onscroll3").onscroll = function () {
+    onScroll3()
+};
+
+function onScroll3() {
+    document.getElementById("demo3").innerHTML = "You scrolled in div.";
+}
+
+document.getElementById("onscroll4").addEventListener("scroll", onScroll4);
+
+function onScroll4() {
+    document.getElementById("demo4").innerHTML = "You have scrolled in div.";
+}
